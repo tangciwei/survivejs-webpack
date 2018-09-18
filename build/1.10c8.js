cf410f3652a8ad6a14d4754244bf0029779d6c8d
tangciwei <tangciwei@baidu.com>
2018-09-18 19:29:42 +0800

    tree-shake

--- build/1.10c8.js
+++ build/1.10c8.js
@@ -1,6 +1,2 @@
-/*! 2dd4d43 */
-(window.webpackJsonp = window.webpackJsonp || []).push([[1], {
-    12: function (o, n, w) {
-        'use strict';w.r(n), n.default = 'Hello from lazy';
-    }
-}]);
+/*! c99c628 */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{12:function(o,n,w){"use strict";w.r(n),n.default="Hello from lazy"}}]);
--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import 'react';
 import 'react-dom';
 import './main.css';
 import component from './component';
-
+// Tree Shaking
+import {bake} from './shake';
+bake();
 document.body.appendChild(component());
